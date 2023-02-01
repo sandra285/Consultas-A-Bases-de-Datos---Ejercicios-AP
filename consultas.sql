ba65d12240a1bef618d95c31cbb2750d03451e33
sandra285 <sandrafabrizi87@gmail.com>
2023-02-01 09:06:09 -0300

    Se agregan Ejercicios 21 a 48

--- consultas.sql
+++ consultas.sql
@@ -79,3 +79,115 @@ WHERE compania NOT IN ('TELCEL', 'IUSACELL');
 /* 20 - Listar el login y teléfono de los usuarios con compañía telefónica UNEFON */
 SELECT usuario, telefono FROM usuarios
 WHERE compania = 'UNEFON';
+
+/* 21 - Listar las diferentes marcas de celular en orden alfabético descendentemente */
+SELECT DISTINCT marca FROM usuarios
+ORDER BY marca DESC;
+
+/* 22 - Listar las diferentes compañías en orden alfabético aleatorio */
+SELECT DISTINCT marca FROM usuarios
+ORDER BY RAND();
+
+/* 23 - Listar el login de los usuarios con nivel 0 o 2 */
+SELECT usuario FROM usuarios
+WHERE nivel IN (0, 2);
+
+/* 24 - Calcular el saldo promedio de los usuarios que tienen teléfono marca LG */
+SELECT AVG(saldo) FROM usuarios
+WHERE marca = 'LG';
+
+/* 25 - Listar el login de los usuarios con nivel 1 o 3 */
+SELECT usuario FROM usuarios
+WHERE nivel IN (1, 3);
+
+/* 26 - Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca BLACKBERRY */
+SELECT nombre, telefono FROM usuarios
+WHERE marca <> 'BLACKBERRY';
+
+/* 27 - Listar el login de los usuarios con nivel 3 */
+SELECT usuario FROM usuarios
+WHERE nivel = 3;
+
+/* 28 - Listar el login de los usuarios con nivel 0 */
+SELECT usuario FROM usuarios
+WHERE nivel = 0;
+
+/* 29 - Listar el login de los usuarios con nivel 1 */
+SELECT usuario FROM usuarios
+WHERE nivel = 1;
+
+/* 30 - Contar el número de usuarios por sexo */
+SELECT sexo, COUNT(*) FROM usuarios
+GROUP BY sexo;
+
+/* 31 - Listar el login y teléfono de los usuarios con compañía telefónica AT&T */
+SELECT usuario, telefono FROM usuarios
+WHERE compania = 'AT&T';
+
+/* 32 - Listar las diferentes compañías en orden alfabético descendentemente */
+SELECT DISTINCT compania FROM usuarios
+ORDER BY compania DESC;
+
+/* 33 - Listar el login de los usuarios inactivos */
+SELECT usuario FROM usuarios
+WHERE activo = 0;
+
+/* 34 - Listar los números de teléfono sin saldo */
+SELECT telefono FROM usuarios
+WHERE saldo = 0;
+
+/* 35 - Calcular el saldo mínimo de los usuarios de sexo “Hombre” */
+SELECT MIN(saldo) FROM usuarios
+WHERE sexo = 'H';
+
+/* 36 - Listar los números de teléfono con saldo mayor a 300 */
+SELECT telefono FROM usuarios
+WHERE saldo > 300;
+
+/* 37 - Contar el número de usuarios por marca de teléfono */
+SELECT marca, COUNT(usuario) FROM usuarios
+GROUP BY marca;
+
+/* 38 - Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca LG */
+SELECT nombre, telefono FROM usuarios
+WHERE marca <> 'LG';
+
+/* 39 - Listar las diferentes compañías en orden alfabético ascendentemente */
+SELECT DISTINCT compania FROM usuarios
+ORDER BY compania;
+
+/* 40 - Calcular la suma de los saldos de los usuarios de la compañía telefónica UNEFON */
+SELECT compania, SUM(saldo) FROM usuarios
+WHERE compania = 'UNEFON';
+
+/*41 - Mostrar el email de los usuarios que usan hotmail */
+SELECT email FROM usuarios
+WHERE email LIKE '%hotmail%';
+
+/* 42 - Listar los nombres de los usuarios sin saldo o inactivos */
+SELECT nombre FROM usuarios
+WHERE saldo = 0 || NOT activo;
+
+/* 43 - Listar el login y teléfono de los usuarios con compañía telefónica IUSACELL o TELCEL */
+SELECT usuario, telefono FROM usuarios
+WHERE compania IN ('IUSACELL', 'TELCEL');
+
+/* 44 - Listar las diferentes marcas de celular en orden alfabético ascendentemente */
+SELECT DISTINCT marca FROM usuarios
+ORDER BY marca;
+
+/* 45 - Listar las diferentes marcas de celular en orden alfabético aleatorio */
+SELECT DISTINCT marca FROM usuarios
+ORDER BY RAND();
+
+/* 46 - Listar el login y teléfono de los usuarios con compañía telefónica IUSACELL o UNEFON */
+SELECT usuario, telefono FROM usuarios
+WHERE compania IN ('IUSACELL', 'UNEFON');
+
+/* 47 - Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca MOTOROLA o NOKIA */
+SELECT nombre, telefono FROM usuarios
+WHERE MARCA NOT IN ('MOTOROLA', 'NOKIA');
+
+/* 48 - Calcular la suma de los saldos de los usuarios de la compañía telefónica TELCEL */
+SELECT compania, SUM(saldo) FROM usuarios
+WHERE compania = 'TELCEL';
